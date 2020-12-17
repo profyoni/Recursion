3c56bdc271d71a4e313000b5dc2b19f100aab1f0
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2020-12-16 20:26:49 -0500

    toString, equals, hashCode

--- src/edu/touro/cs/Recursion.java
+++ src/edu/touro/cs/Recursion.java
@@ -21,16 +21,31 @@ public class Recursion {
     {
        if (n == 0)
            return 1; // base case
-       int ret = n * factorial(n-1); // recursive case
+       int rec = factorial(n-1);
+       int ret = n * rec; // recursive case
        return ret;
     }
+
+        public static long fibIter(int num){
+        long a = 0;
+        long b = 1;
+        for (int i = 0; i < num -1; i++) {
+            long temp = b;
+            b += a;
+            a = temp;
+        }
+        return b;
+    }
+
     public static int fibO1(int n) {
         double a = (1 + Math.sqrt(5))/2,
                b = (1 - Math.sqrt(5))/2;
 
         return (int) Math.round((Math.pow(a,n) - Math.pow(b,n))/Math.sqrt(5));
     }
+
     private static HashMap<Integer, Integer> memoizedFib = new HashMap<>();
+
     public static int fib(int n)
     {
         // memoize result from a previous
@@ -44,17 +59,26 @@ public class Recursion {
         return ret;
     }
 
+    private static boolean isPalindrome3(char [] s, int startIndex, int endIndex) {
+        if (endIndex - startIndex <= 1) // 0 or 1 characters
+            return true;
+
+        return s[startIndex] == s[endIndex-1] &&
+                isPalindrome3(s, startIndex + 1, endIndex-1);
+    }
+
+    public static boolean isPalindrome3(String s) {
+        return isPalindrome3(s.toCharArray(), 0, s.length());
+    }
 
     public static boolean isPalindrome2(String s) {
-        for( int startIndex = 0, endIndex = s.length()-1;
-             startIndex < endIndex;
-             startIndex++, endIndex--)
-        {
-            if (s.charAt(startIndex) != s.charAt(endIndex))
-                return false;
-        }
-        return true;
+        if (s.length() <= 1)
+            return true;
+
+        return s.charAt(0) == s.charAt(s.length()-1) &&
+                isPalindrome2(s.substring(1,s.length()-1));//1 million chars...creating 1/2 million string... O(n^2) space
     }
+
     public static boolean isPalindrome(String s) {
         // Base case "", 1 character
         // Recurive 2+ characters
--- tests/edu/touro/cs/RecursionTest.java
+++ tests/edu/touro/cs/RecursionTest.java
@@ -8,18 +8,44 @@ class RecursionTest {
 
     @Test
     void factorial() {
-        int actual = Recursion.factorial(995);
+        int actual = Recursion.factorial(155);
 
         assertEquals(120, actual);
     }
+
     @Test
     void fib() {
-        int actual = Recursion.fib(50);
+        long actual = Recursion.fib(50);
 
         assertEquals(55, actual);
     }
 
     @Test
     void isPalindrome2() {
+        String s = "abba";
+        //Arrange, Act, Assert
+        boolean actual = Recursion.isPalindrome2(s);
+
+        assertEquals(true, actual);
+    }
+
+
+    @Test
+    void isPalindrome2_many() {
+        assertTrue(Recursion.isPalindrome2("abba"));
+        assertTrue(Recursion.isPalindrome2("racecar"));
+        assertTrue( Recursion.isPalindrome2("q"));
+
+        assertTrue(! Recursion.isPalindrome2("qwerty"));
+        assertTrue(! Recursion.isPalindrome2("asdwsa"));
+    }
+    @Test
+    void isPalindrome3_many() {
+        assertTrue(Recursion.isPalindrome3("abba"));
+        assertTrue(Recursion.isPalindrome3("racecar"));
+        assertTrue( Recursion.isPalindrome3("q"));
+
+        assertTrue(! Recursion.isPalindrome3("qwerty"));
+        assertTrue(! Recursion.isPalindrome3("asdwsa"));
     }
 }
